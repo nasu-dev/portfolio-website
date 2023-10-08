13cd36bb159a1938d5704b309eb2f1d059056b0e
Aki <sekai.110.0604@gmail.com>
2023-10-08 12:49:57 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import About from '@/components/about'
+import Contact from '@/components/contact'
+import Experience from '@/components/experience'
+import Intro from '@/components/intro'
+import Projects from '@/components/projects'
+import SectionDevider from '@/components/section-devider'
+import Skills from '@/components/skills'
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center px-6">
+      <Intro />
+      <SectionDevider />
+      <About />
+      <Projects />
+      <Skills />
+      <Experience />
+      <Contact />
+    </main>
+  )
+}
---
+++ next.config.js
@@ -0,0 +1,18 @@
+const { hostname } = require('os')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            }
+        ]
+    },
+    experimental: {
+        serverActions: true,
+    }
+}
+
+module.exports = nextConfig
